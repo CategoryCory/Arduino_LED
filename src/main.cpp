219743d92fa95ecc8006d4eeb679f0d21a16d3db
Cory Coward <ccoward80@gmail.com>
2024-02-14 21:30:28 -0500

    Add HSV to LCD display

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,31 @@
 #define SAT_PIN     A1
 #define VAL_PIN     A2
 #define NUM_PIXELS  1
-#define LCD_RS      12
-#define LCD_ENABLE  11
-#define LCD_DB4     5
-#define LCD_DB5     4
-#define LCD_DB6     3
-#define LCD_DB7     2
+#define LCD_RS      2
+#define LCD_ENABLE  3
+#define LCD_DB4     8
+#define LCD_DB5     9
+#define LCD_DB6     10
+#define LCD_DB7     11
 
 float hue = 0;
 float sat = 0;
 float val = 0;
+
+char hsv[32] = {};
+char rgb[32] = {};
 
 Adafruit_NeoPixel pixels(NUM_PIXELS, LED_PIN, NEO_RGB);
 LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
 
 void setup()
 {
-  Serial.begin(9600);
   pixels.begin();
   pixels.clear();
   lcd.begin(16, 2);
-  lcd.print("Hello World");
 }
 
-void loop() 
+void loop()
 {
   hue = (analogRead(HUE_PIN) / 1023.0) * 65535;
   sat = (analogRead(SAT_PIN) / 1023.0) * 255;
@@ -40,14 +41,10 @@ void loop()
   pixels.setPixelColor(0, pixels.ColorHSV(hue, sat, val));
   pixels.show();
 
-  Serial.print("HSV(");
-  Serial.print(hue / 65535.0 * 360);
-  Serial.print(", ");
-  Serial.print(sat / 255.0 * 100);
-  Serial.print(", ");
-  Serial.print(val / 255.0 * 100);
-  Serial.print(")");
-  Serial.println();
+  sprintf(hsv, "HSV(%d,%d,%d)", (int)(hue / 65535.0 * 360), (int)(sat / 255.0 * 100), (int)(val / 255.0 * 100));
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(hsv);
 
   delay(1000);
 }
